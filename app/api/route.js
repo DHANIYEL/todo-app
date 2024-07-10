7ef16f7557b2b73c3d92d460463abab393fcfa2b
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-07-10 18:56:18 +0530

    db error

--- app/api/route.js
+++ app/api/route.js
@@ -1,13 +1,22 @@
 import { NextResponse } from "next/server";
 import { ConnectDB } from "../../lib/config/db";
+import { TodoModel } from "@/lib/model/TodoModel";
 
 const LoadDB = async () => {
-    await ConnectDB()
-}
+  await ConnectDB();
+};
 
 LoadDB();
 
-export async function GET(request){
-      toast.success("Success")
-    return NextResponse.json({msg:"Get msg"})
+export async function GET(request) {
+  return NextResponse.json({ msg: "Get msg" });
+}
+export async function POST(request) {
+  const { title, description } = await request.json();
+  await TodoModel.create({
+    title,
+    description
+  })
+
+  return NextResponse.json({ msg: "Todo created" });
 }
