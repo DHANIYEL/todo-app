1333a5dbac44112c5cbc4b1604e161f063748e9a
DHANIYEL <dhaniyeldarvesh7@gmail.com>
2024-07-10 13:35:17 +0530

    db

--- app/api/route.js
+++ app/api/route.js
@@ -1,5 +1,13 @@
 import { NextResponse } from "next/server";
+import { ConnectDB } from "../../lib/config/db";
+
+const LoadDB = async () => {
+    await ConnectDB()
+}
+
+LoadDB();
 
 export async function GET(request){
+      toast.success("Success")
     return NextResponse.json({msg:"Get msg"})
 }
